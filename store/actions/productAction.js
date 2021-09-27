b7bd0bb120f79251cea1765043546a2fd308ff7e
Lyqueen <58933390+fikrullaily@users.noreply.github.com>
2021-09-27 12:05:15 +0700

    Update productAction.js
    datanya masih belum API

--- store/actions/productAction.js
+++ store/actions/productAction.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 import * as types from '../types'
 
 export const fetchproducts =()=>async dispatch=>{
-    const res = await axios.get('https://fakestoreapi.com/products');
+//     const res = await axios.get('https://fakestoreapi.com/products');
     dispatch({
         type: types.GET_PRODUCTS,
-        payload: res.data,
+        payload: ['1st post', '2nd post']
     });
 }
