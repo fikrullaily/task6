a466b2ac6e1c6de11bf8537d3a0668e1edb0caf5
Lyqueen <58933390+fikrullaily@users.noreply.github.com>
2021-10-01 12:15:26 +0700

    Update Product.js

--- pages/Product.js
+++ pages/Product.js
@@ -6,17 +6,18 @@ import Image from "next/image";
 const Product = (props) => {
     const { handleEdit } = props;
     const dispatch = useDispatch();
-    const { product } = useSelector((state) => state.post);
-    const {loading, error, products} = product;
+    const  Products = useSelector((state) => state.Products);
+    // const { ProductsData } = useSelector((state) => state.Product);
+    const { loading, error, products } = Products;
 
     useEffect(() => {
         dispatch(fetchproducts());
     }, []);
 
-    const handleDelete = (id) => {
-        dispatch(deleteProduct(id));
-        alert("has been deleted");
-    };
+    // const handleDelete = (id) => {
+    //     dispatch(deleteProduct(id));
+    //     alert("has been deleted");
+    // };
 
     return(
         <section className="getproduct">
@@ -26,18 +27,25 @@ const Product = (props) => {
                 ? error.message
                 : products?.map((product) => (
                     <div className="product">
-                    <div className="image">
+                    {/* <div className="image">
                         <Image src={product.image} alt={product.image}
                             width={200} height={250} />
-                    </div>
+                    </div> */}
                     <div className="text">
                         <h3 key={product.id}>{product.title}</h3>
                         <h4>$ {product.price}</h4>
+                        <p>{product.description}</p>
                         <h4>{product.category}</h4>
                     </div>
                     <div>
                         <button onClick={() => handleEdit(product)}>Edit</button>
-                        <button onClick={() => handleDelete(product.id)}>Delete</button>   
+                        {/* <button onClick={() => handleDelete(product.id)}>Delete</button>    */}
+                        <button onClick={() => 
+                            dispatch(
+                                deleteProduct(product.id),
+                                alert("Deleted Product " + product.title )
+                            )    
+                        }>Delete</button>
                     </div>
                     </div>
                 ))}
